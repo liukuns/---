bbeec7c17fa761c04324b3cfa9cf9bcee997016f
Lk <128580641+liukuns@users.noreply.github.com>
2024-03-14 21:02:29 +0800

    Update dag.go

--- dag.go
+++ dag.go
@@ -1,113 +1,92 @@
 package merkledag
 
-import "hash"
-
-func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return nil
-}
-
-// package merkledag
-
-// import (
-// 	"hash"
-// )
-
-// func Add(store KVStore, node Node, h hash.Hash) []byte {
-// 	switch node.Type() {
-// 	case FILE:
-// 		// 对于文件，我们直接将其内容保存到KVStore中
-// 		file := node.(File)
-// 		content := file.Bytes()
-// 		h.Write(content)
-// 		hash := h.Sum(nil)
-// 		store.Put(hash, content)
-// 		return hash
-// 	case DIR:
-// 		// 对于目录，我们需要遍历其下的所有节点，并递归地调用Add函数
-// 		dir := node.(Dir)
-// 		it := dir.It()
-// 		var hashes []byte
-// 		for it.Next() {
-// 			hash := Add(store, it.Node(), h)
-// 			hashes = append(hashes, hash...)
-// 		}
-// 		// 最后，我们将所有子节点的哈希值连接起来，并计算其哈希值
-// 		h.Reset()
-// 		h.Write(hashes)
-// 		hash := h.Sum(nil)
-// 		store.Put(hash, hashes)
-// 		return hash
-// 	default:
-// 		// 未知的节点类型
-// 		return nil
-// 	}
-// }
-
-
-
-package merkledag
-
 import (
+	"encoding/json"
 	"hash"
 )
 
-// 定义一个新的哈希结构
-type MyHash struct {
-	data []byte
+type Link struct {
+	Name string
+	Hash []byte
+	Size int
 }
 
-// 为新的哈希结构实现 hash.Hash 接口的所有方法
-func (h *MyHash) Write(p []byte) (n int, err error) {
-	h.data = append(h.data, p...)
-	return len(p), nil
+type Object struct {
+	Links []Link
+	Data  []byte
 }
 
-func (h *MyHash) Sum(b []byte) []byte {
-	return append(b, h.data...)
-}
-
-func (h *MyHash) Reset() {
-	h.data = nil
-}
-
-func (h *MyHash) Size() int {
-	return len(h.data)
-}
-
-func (h *MyHash) BlockSize() int {
-	return 1
-}
-
-// 在 Add 函数中使用新的哈希结构
-func Add(store KVStore, node Node) []byte {
-	h := &MyHash{}
+func Add(store KVStore, node Node, h hash.Hash) []byte {
 	switch node.Type() {
-	case FILE:
-		// 对于文件，我们直接将其内容保存到KVStore中
-		file := node.(File)
-		content := file.Bytes()
-		h.Write(content)
-		hash := h.Sum(nil)
-		store.Put(hash, content)
-		return hash
 	case DIR:
-		// 对于目录，我们需要遍历其下的所有节点，并递归地调用Add函数
-		dir := node.(Dir)
-		it := dir.It()
-		var hashes []byte
-		for it.Next() {
-			hash := Add(store, it.Node())
-			hashes = append(hashes, hash...)
-		}
-		// 最后，我们将所有子节点的哈希值连接起来，并计算其哈希值
-		h.Reset()
-		h.Write(hashes)
-		hash := h.Sum(nil)
-		store.Put(hash, hashes)
+		file := node.(File)
+		tmp := StoreFile(store, file, h)
+		jsonMarshal, _ := json.Marshal(tmp)
+		hash := calculateHash(jsonMarshal, h)
 		return hash
-	default:
-		// 未知的节点类型
-		return nil
+	case FILE:
+		dir := node.(Dir)
+		tmp := StoreDir(store, dir, h)
+		jsonMarshal, _ := json.Marshal(tmp)
+		hash := calculateHash(jsonMarshal, h)
+		return hash
 	}
+	panic("unknown node")
+}
+
+func calculateHash(data []byte, h hash.Hash) []byte {
+	h.Reset()
+	hash := h.Sum(data)
+	h.Reset()
+	return hash
+}
+
+func StoreFile(store KVStore, file File, h hash.Hash) *Object {
+	data := file.Bytes()
+	blob := Object{Data: data, Links: nil}
+	jsonMarshal, _ := json.Marshal(blob)
+	hash := calculateHash(jsonMarshal, h)
+	store.Put(hash, data)
+	return &blob
+}
+
+func StoreDir(store KVStore, dir Dir, h hash.Hash) *Object {
+	it := dir.It()
+	treeObject := &Object{}
+	for it.Next() {
+		n := it.Node() //当前目录下的node
+		switch n.Type() {
+		case FILE:
+			file := n.(File)
+			tmp := StoreFile(store, file, h)
+			jsonMarshal, _ := json.Marshal(tmp)
+			hash := calculateHash(jsonMarshal, h)
+			treeObject.Links = append(treeObject.Links, Link{
+				Hash: hash,
+				Size: int(file.Size()),
+				Name: file.Name(),
+			})
+			typeName := "link"
+			if tmp.Links == nil {
+				typeName = "blob"
+			}
+			treeObject.Data = append(treeObject.Data, []byte(typeName)...)
+		case DIR:
+			dir := n.(Dir)
+			tmp := StoreDir(store, dir, h)
+			jsonMarshal, _ := json.Marshal(tmp)
+			hash := calculateHash(jsonMarshal, h)
+			treeObject.Links = append(treeObject.Links, Link{
+				Hash: hash,
+				Size: int(dir.Size()),
+				Name: dir.Name(),
+			})
+			typeName := "tree"
+			treeObject.Data = append(treeObject.Data, []byte(typeName)...)
+		}
+	}
+	jsonMarshal, _ := json.Marshal(treeObject)
+	hash := calculateHash(jsonMarshal, h)
+	store.Put(hash, jsonMarshal)
+	return treeObject
 }
